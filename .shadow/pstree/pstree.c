bab436d62e7b4f9d54aa5a85be09c6097ef5919e
tracer-nju <tracer@nju.edu.cn>
2025-03-18 22:43:27 +0800

    Linux zhang 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:43:27 up 32 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,146 +1,79 @@
 #include <stdio.h>
-#include <assert.h>
-#include <string.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <dirent.h>
 #include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-struct data {
-    char name[100];
-    int pid;
-    int ppid;
-    int layer;
-    char pidstring[100];
-};
+#define MAX_PROCS 1024
 
-struct Node {
-    struct data item;
-    struct Node* parent;
-    struct Node* child;
-    struct Node* brother;
-    int layer;
-};
+typedef struct Process {
+    pid_t pid;
+    pid_t ppid;
+    char name[256];
+} Process;
 
-void print(struct Node* node, bool show_pid) {
-    if (!node) return;
+Process procs[MAX_PROCS];
+int proc_count = 0;
 
-    for (int i = 0; i < node->layer; i++)
-        printf("\t");
+void read_proc() {
+    DIR *dir;
+    struct dirent *entry;
 
-    if (show_pid) {
-        printf("%s(%s)\n", node->item.name, node->item.pidstring);
-    } else {
-        printf("%s\n", node->item.name);
+    if ((dir = opendir("/proc")) == NULL) {
+        perror("opendir(/proc) failed");
+        exit(EXIT_FAILURE);
     }
 
-    print(node->child, show_pid);
-    print(node->brother, show_pid);
-}
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char *endptr;
+            pid_t pid = strtol(entry->d_name, &endptr, 10);
+            if (*endptr == '\0') {  // if it's a number
+                char path[256];
+                sprintf(path, "/proc/%d/stat", pid);
 
-int compute_layer(struct data* a, struct data* list, int size) {
-    if (a->layer >= 0) return a->layer;
-    if (a->ppid == 0) return (a->layer = 0);
-
-    for (int i = 0; i < size; i++) {
-        if (list[i].pid == a->ppid) {
-            a->layer = compute_layer(&list[i], list, size) + 1;
-            return a->layer;
-        }
-    }
-    return (a->layer = 0);
-}
-
-int compare_alpha(const void *a, const void *b) {
-    struct data *d1 = (struct data*)a;
-    struct data *d2 = (struct data*)b;
-    if (d1->layer != d2->layer) return d1->layer - d2->layer;
-    return strcmp(d1->name, d2->name);
-}
-
-int compare_numeric(const void *a, const void *b) {
-    struct data *d1 = (struct data*)a;
-    struct data *d2 = (struct data*)b;
-    if (d1->layer != d2->layer) return d1->layer - d2->layer;
-    return d1->pid - d2->pid;
-}
-
-int filter(const struct dirent* dir) {
-    return dir->d_name[0] >= '0' && dir->d_name[0] <= '9';
-}
-
-int main(int argc, char *argv[]) {
-    bool show_pids = false, numeric_sort = false, show_version = false;
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-p") == 0) show_pids = true;
-        if (strcmp(argv[i], "-n") == 0) numeric_sort = true;
-        if (strcmp(argv[i], "-V") == 0) show_version = true;
-    }
-
-    if (show_version) {
-        printf("pstree (PSmisc) 23.1\n");
-        return 0;
-    }
-
-    struct dirent **namelist;
-    int n = scandir("/proc", &namelist, filter, alphasort);
-    struct data list[n];
-
-    for (int i = 0; i < n; i++) {
-        char path[100], line[100];
-        sprintf(path, "/proc/%s/status", namelist[i]->d_name);
-        FILE* fp = fopen(path, "r");
-        if (!fp) continue;
-
-        fgets(line, 100, fp);
-        strcpy(list[i].name, line + 6);
-        list[i].name[strcspn(list[i].name, "\n")] = 0;
-
-        while (fgets(line, 100, fp) && strncmp(line, "Pid:", 4) != 0);
-        sscanf(line + 5, "%d", &list[i].pid);
-        sprintf(list[i].pidstring, "%d", list[i].pid);
-
-        while (fgets(line, 100, fp) && strncmp(line, "PPid:", 5) != 0);
-        sscanf(line + 6, "%d", &list[i].ppid);
-
-        list[i].layer = -1;
-        fclose(fp);
-    }
-
-    for (int i = 0; i < n; i++)
-        compute_layer(&list[i], list, n);
-
-    qsort(list, n, sizeof(struct data), numeric_sort ? compare_numeric : compare_alpha);
-
-    struct Node nodes[n];
-    memset(nodes, 0, sizeof(nodes));
-    for (int i = 0; i < n; i++) {
-        nodes[i].item = list[i];
-        nodes[i].layer = list[i].layer;
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (nodes[i].item.ppid == 0) continue;
-        for (int j = 0; j < n; j++) {
-            if (nodes[j].item.pid == nodes[i].item.ppid) {
-                nodes[i].parent = &nodes[j];
-                if (!nodes[j].child) {
-                    nodes[j].child = &nodes[i];
-                } else {
-                    struct Node* temp = nodes[j].child;
-                    while (temp->brother) temp = temp->brother;
-                    temp->brother = &nodes[i];
+                FILE *stat_file = fopen(path, "r");
+                if (stat_file) {
+                    Process p;
+                    p.pid = pid;
+                    fscanf(stat_file, "%d %s %*c %d", &p.pid, p.name, &p.ppid);
+                    fclose(stat_file);
+                    procs[proc_count++] = p;
                 }
-                break;
             }
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        if (nodes[i].item.pid == 1) {
-            print(&nodes[i], show_pids);
-            break;
+    closedir(dir);
+}
+
+void print_tree(pid_t ppid, int indent, int is_last) {
+    for (int i = 0; i < proc_count; i++) {
+        if (procs[i].ppid == ppid) {
+            for (int j = 0; j < indent; j++) {
+                if (j == indent - 1) {
+                    printf("%s", is_last ? "└──" : "├──");
+                } else {
+                    printf("│   ");
+                }
+            }
+            printf("%s\n", procs[i].name);
+
+            int has_child = 0;
+            for (int k = 0; k < proc_count; k++) {
+                if (procs[k].ppid == procs[i].pid) {
+                    has_child = 1;
+                    break;
+                }
+            }
+            print_tree(procs[i].pid, indent + 1, !has_child);
         }
     }
+}
+
+int main(int argc, char *argv[]) {
+    read_proc();
+    print_tree(1, 0, 0);  // 从init进程（PID 1）开始
     return 0;
 }
