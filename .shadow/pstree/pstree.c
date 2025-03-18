93f10f952e80b7a500a2719b33726398ef9af5bd
tracer-nju <tracer@nju.edu.cn>
2025-03-18 22:31:54 +0800

    Linux zhang 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:31:54 up 20 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,288 +1,308 @@
-
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <dirent.h>
-#include <sys/stat.h>
 #include <stdlib.h>
-typedef struct pidinfo{
-    char name[50];
-    __pid_t pid;
-    __pid_t ppid;
-}PidInfo;
-PidInfo pidinfos[10000];
-int pid_count=0;
-char* readcmdops(int argc, char *argv[])//get commandline ops
-{
-    assert(argc>=1);
-    if(argc==1)//no ops
-    {
-        return NULL;
-    }
-    char *p=argv[1];
-    char ops[10];int j=0;
-    for(int i=0;i<strlen(p);i++)
-    {
-        if(p[i]=='-')
-        {
-            continue;
-        }
-        ops[j]=p[i];
-        j++;
-    }
-    strcpy(p,ops);
-    return p;
-}
-pid_t readprocessname_ppid(__pid_t pid,char name[])
-{
-    char *str=(char *)malloc(sizeof(char)*20);
-    if(str==NULL)
-    {
-        printf("malloc failed");
-        exit(1);
-    }
-    sprintf(str,"%d",pid);
-    char processpath[20]="/proc/";
-    strcat(processpath,str);
-    strcat(processpath,"/stat");
-    FILE *fp=fopen(processpath,"r");
-    if (fp){
-        char i;
-        __pid_t _pid,ppid;
-        char pname[40];
-        fscanf(fp,"%d (%s %c %d",&_pid,pname,&i,&ppid);
-        pname[strlen(pname)-1]='\0';
-        strcpy(name,pname);
-        assert(pid==_pid);
-        fclose(fp);
-        free(str);
-        return ppid;
-    }else{
-        printf("open failed");
-        free(str);
-        exit(1);
-    }
-}
-void setProcessInfo()
-{
-    DIR *dp=opendir("/proc");
-    if(!dp)
-    {
-        fprintf(stderr,"%s","Can 't open /proc/");
-        exit(1);
-    }
-    else
-    {
-        int pid=0;
-        struct dirent *entry;
-        while ((entry=readdir(dp))!=NULL)
-        {
-            if((pid=atoi(entry->d_name))==0)
-            {
-                continue;
-            }
-            else{// store in pidinfo (name and pid and ppid)
-                pidinfos[pid_count].pid=pid;
-                pidinfos[pid_count].ppid=readprocessname_ppid(pid,pidinfos[pid_count].name);
-                assert(pidinfos[pid_count].ppid>-1);
-                //printf("%d (%s) %d\n",pidinfos[pid_count].pid,pidinfos[pid_count].name,pidinfos[pid_count].ppid);
-                pid_count++;
-            }
-        }
-    }
-}
+#include <assert.h>
 
-typedef struct processtree{
-    pid_t pid;
-    char name[40];
-    struct childprocesses *children;
-}Processtree;
-struct childprocesses{
-    struct childprocesses *next;
-    Processtree *proc;
+
+struct data{
+    char name[100];
+    int pid;
+    int ppid;
+    int layer;
+    char pidstring[100];
+    char ppidstring[100];
 };
 
-void findallchildrens(int pid ,int index[])
-{
-    int indexcount=0;
-    size_t i= 0;
-    for (; i <pid_count-1; i+=2)
-    {
-        if(pidinfos[i].ppid==pid)
-        {
-            index[indexcount++]=i;
-        }
-        if(pidinfos[i+1].ppid==pid)
-        {
-            index[indexcount++]=i+1;
-        }
-    }
-    for (; i <pid_count; i++)
-    {
-        if(pidinfos[i].ppid==pid)
-        {
-            index[indexcount++]=i;
-        }
-    }
-}
-void creat_tree(Processtree * root,int tab_length)
-{
-    int flag=2;
-    char str[100]={0};
-    int allchildrensindex[500]={0}; // Log the index of all root child processes in Pidinfos
-    findallchildrens(root->pid,allchildrensindex);
-    root->children=(struct childprocesses*)malloc(sizeof(struct childprocesses));
-    if(root->children==NULL)
-    {
-        printf("malloc failed");
-        exit(1);
-    }
-    struct childprocesses *child=root->children;
-    if(allchildrensindex[0]==0)//it's leaf node,no child,return
-    {
-        printf("%s(%d)",root->name,root->pid);
-        return;
-    }
-    sprintf(str,"%s(%d)-",root->name,root->pid);
-    printf("%s",str);
-    if(allchildrensindex[1]!=0)
-    {
-        printf("+-");
-    }
-    else
-    {
-        flag=0;
-    }
-    for(int i=0;i<500 && allchildrensindex[i]!=0 ;i++)//Traverses all children of the process
-    {
-        child->proc=(Processtree *)malloc(sizeof(Processtree));
-        if(child->proc==NULL)
-        {
-            printf("malloc failed");
-            exit(1);
-        }
-        child->proc->pid=pidinfos[allchildrensindex[i]].pid;
-        strcpy(child->proc->name,pidinfos[allchildrensindex[i]].name);
-        creat_tree(child->proc,strlen(str)+tab_length+flag);   // recursive
-        if(i+1<500 && allchildrensindex[i+1]!=0)
-        {
-            child->next=(struct childprocesses*)malloc(sizeof(struct childprocesses));
-            if(child->next==NULL)
-            {
-                printf("malloc failed");
-                exit(1);
-            }
-            child=child->next;
-            char tabs[100];
-            printf("\n");
-            for (size_t i = 0; i <strlen(str)+tab_length; i++)
-            {
-                printf(" ");
-            }
-            printf("|-");
-        }
-    }
-    free(root);
-}
-void creat_tree_nopid(Processtree * root,int tab_length)
-{
-    int flag=2;
-    char str[100]={0};
-    int allchildrensindex[500]={0}; // Log the index of all root child processes in Pidinfos
-    findallchildrens(root->pid,allchildrensindex);
-    root->children=(struct childprocesses*)malloc(sizeof(struct childprocesses));
-    if(root->children==NULL)
-    {
-        printf("malloc failed");
-        exit(1);
-    }
-    struct childprocesses *child=root->children;
-    if(allchildrensindex[0]==0)//it's leaf node,no child,return
-    {
-        printf("%s",root->name);            //TODO 缺陷列表2  leafnode task:: /proc/pid/task/.../stat
-        return;
-    }
-    sprintf(str,"%s-",root->name);
-    printf("%s",str);
-    if(allchildrensindex[1]!=0)
-    {
-        printf("+-");
-    }
-    else
-    {
-        flag=0;
-    }
-    for(int i=0;i<500 && allchildrensindex[i]!=0 ;i++)//Traverses all children of the process
-    {
-        child->proc=(Processtree *)malloc(sizeof(Processtree));
-        if(child->proc==NULL)
-        {
-            printf("malloc failed");
-            exit(1);
-        }
-        child->proc->pid=pidinfos[allchildrensindex[i]].pid;
-        strcpy(child->proc->name,pidinfos[allchildrensindex[i]].name);
-        creat_tree_nopid(child->proc,strlen(str)+tab_length+flag);   // recursive
-        if(i+1<500 && allchildrensindex[i+1]!=0)
-        {
-            child->next=(struct childprocesses*)malloc(sizeof(struct childprocesses));
-            if(child->next==NULL)
-            {
-                printf("malloc failed");
-                exit(1);
-            }
-            child=child->next;
-            char tabs[100];
-            printf("\n");
-            for (size_t i = 0; i <strlen(str)+tab_length; i++)
-            {
-                printf(" ");
-            }
-            printf("|-");
-        }
-    }
-    free(root);
-}
-int main(int argc, char *argv[]) {
-    char* ops=readcmdops(argc,argv);//ops是选项
-    if(!ops)//ops为空时 默认
-    {
-        setProcessInfo();
-        Processtree *root=(Processtree *)malloc(sizeof(Processtree));
-        root->pid=pidinfos[0].pid;
-        strcpy(root->name,pidinfos[0].name);
-        creat_tree_nopid(root,0);
-    }
-    else
-    {
-        if(!strcmp(ops,"p"))   //ops = -p
-        {
-            //printf("%s",ops);
-            setProcessInfo();
-            Processtree *root=(Processtree *)malloc(sizeof(Processtree));
-            root->pid=pidinfos[0].pid;
-            strcpy(root->name,pidinfos[0].name);
-            creat_tree(root,0);
-        }
-        else if(!strcmp(ops,"n"))   //  -n
-        {
-            //printf("%s",ops);
-            setProcessInfo();
-            Processtree *root=(Processtree *)malloc(sizeof(Processtree));
-            root->pid=pidinfos[0].pid;
-            strcpy(root->name,pidinfos[0].name);
-            creat_tree_nopid(root,0);
+struct Node{
+    struct data item;
+    struct Node* parent;
+    struct Node* child;
+    struct Node* brother;
+    int layer;
+};
 
+void print(struct Node* node, bool show_pid) {
+    if (show_pid == false) {
+        if (node == NULL) {
+            return ;
         }
-        else if(!strcmp(ops,"V"))  //    -V
-        {
-            printf("pstree (PSmisc) UNKNOWN\nCopyright (C) 1993-2019 Werner Almesberger and Craig Small\nPSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under\nthe terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING.");
+        if (node->child == NULL) {
+            for (int i = 0; i < node->layer; i++) {
+                printf("\t");
+            }
+            printf("%s", node->item.name);
+            print(node->brother, show_pid);
+            return;
         }
-        else
-        {
-            printf("%s","wrong ops");
-            return -1;
+        for (int i = 0; i < node->layer; i++)
+            printf("\t");
+        printf("%s", node->item.name);
+        print(node->child, show_pid);
+        print(node->brother, show_pid);
+    } else {
+        if (node == NULL) {
+            return ;
         }
+        for (int i = 0; node->item.name[i] !='\0'; i++) {
+            if (node->item.name[i] == '\n') {
+                node->item.name[i] = '\0';
+            }
+        }
+        char left_bracket[] = "(";
+        char right_bracket[] = ")\n";
+        strcat(strcat(strcat(node->item.name, left_bracket), node->item.pidstring), right_bracket);
+        if (node->child == NULL) {
+            for (int i = 0; i < node->layer; i++) {
+                printf("\t");
+            }
+
+            printf("%s", node->item.name);
+            print(node->brother, show_pid);
+            return;
+        }
+        for (int i = 0; i < node->layer; i++)
+            printf("\t");
+        printf("%s", node->item.name);
+        print(node->child, show_pid);
+        print(node->brother, show_pid);
+
     }
+}
+void swap_data(struct data* pd1, struct data* pd2) {
+    struct data tmp;
+    struct data* ptmp = &tmp;
+    strcpy(ptmp->name, pd1->name);
+    strcpy(ptmp->pidstring, pd1->pidstring);
+    ptmp->pid = pd1->pid;
+    ptmp->ppid = pd1->ppid;
+    ptmp->layer = pd1->layer;
+
+    strcpy(pd1->name, pd2->name);
+    strcpy(pd1->pidstring, pd2->pidstring);
+    pd1->pid = pd2->pid;
+    pd1->ppid = pd2->ppid;
+    pd1->layer = pd2->layer;
+
+    strcpy(pd2->name, ptmp->name);
+    strcpy(pd2->pidstring, ptmp->pidstring);
+    pd2->pid = ptmp->pid;
+    pd2->ppid = ptmp->ppid;
+    pd2->layer = ptmp->layer;
+}
+
+void copy_data(struct data* pd1, struct data* pd2) {
+    strcpy(pd1->name, pd2->name);
+    strcpy(pd1->pidstring, pd2->pidstring);
+    pd1->layer = pd2->layer;
+    pd1->pid = pd2->pid;
+    pd1->ppid = pd2->ppid;
+}
+
+int compute_layer(struct data* a, struct data* list) {
+    if (a->layer > 0)
+        return a->layer;
+    if (a->ppid == 0) {
+        a->layer = 0;
+        return a->layer;
+    }
+
+    else
+    {
+        struct data* pdata;
+        for (pdata = list; pdata->pid != a->ppid; pdata++);
+        assert(pdata->pid == a->ppid);
+        a->layer = compute_layer(pdata, list) + 1;
+        return a->layer;
+    }
+
+}
+
+void showdir(char* path_of_dir) {
+    DIR* dir;
+    struct dirent *ptr;
+    dir = opendir(path_of_dir);
+    while((ptr = readdir(dir)) != NULL) {
+        printf("%s is in the directory %s.\n", ptr->d_name, path_of_dir);
+    }
+}
+
+int my_filter(const struct dirent* dir) {
+    if ((dir->d_name)[0] >= '0' && (dir->d_name)[0]<='9')
+        return 1;
+    else
+        return 0;
+}
+
+
+
+int main(int argc, char *argv[]) {
+    typedef bool pstree_option;
+    pstree_option pstree_show_pids, pstree_numeric_sort, pstree_version;
+    pstree_show_pids = pstree_numeric_sort = pstree_version = false;
+    for (int i  = 0; i < argc; i++) {
+        assert(argv[i]);
+        if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0)
+            pstree_show_pids = true;
+        if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "numeric-sort") == 0)
+            pstree_numeric_sort = true;
+        if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0)
+            pstree_version = true;
+    }
+
+    if (pstree_version == true) {
+        fprintf(stderr, "pstree (PSmisc) 23.1\nCopyright (C) 1993-2017 Werner Almesberger and Craig Small\n\nPSmisc 不提供任何保证。\n该程序为自由软件，欢迎你在 GNU 通用公共许可证 (GPL) 下重新发布。\n详情可参阅 COPYING 文件。\n");
+        return 0;
+    }
+
+
+    struct dirent **namelist;
+    int n;
+    n = scandir("/proc", &namelist, my_filter, alphasort);
+    int nn = n;
+    char current_path[100];
+    char one_line[100];
+    struct data list[nn];
+    for (int i = 0; i < nn; i++) {
+        strcat(strcat(strcpy(current_path,"/proc/"), namelist[i]->d_name), "/status");
+        FILE* fp = fopen(current_path, "r");
+        fgets(one_line, 100, fp);
+        strcpy(list[i].name, one_line + 6);
+        while(strncmp(one_line, "Pid:", 4) != 0) {
+            fgets(one_line, 100, fp);
+        } // get pid
+        strcpy(list[i].pidstring, one_line + 5);
+        // list[i].pidstring[strlen(one_line)-2] = '\0'
+        for (int j = 0; list[i].pidstring[j] != '\0'; j++) {
+            if (list[i].pidstring[j] == '\n') {
+                list[i].pidstring[j] = '\0';
+            }
+        }
+        list[i].pid = atoi(one_line + 5);
+        while (strncmp(one_line, "PPid:", 5) != 0) {
+            fgets(one_line, 100, fp);
+        } // get ppid
+        list[i].ppid = atoi(one_line + 6);
+        list[i].layer = -1;
+        // if (strncmp(list[i].name, "xfce4-terminal", strlen("xfce4-terminal")) == 0 && list[i].pid ==1319) {
+        //     printf("\n\n$%sAWESOME!!!!\ni:%dpid:%d ppid:%dpidstring:%s\nppidstring:%s&\n\n", list[i].name, i, list[i].pid, list[i].ppid,list[i].pidstring, list[i].ppidstring);
+
+        // } // assert(0);
+        // if (strncmp(list[i].name, "bash", 4) == 0)
+        //     printf("list[%d].name is %slist[i].pid is %d\nlist[i].ppid is %d\n\n", i, list[i].name, list[i].pid, list[i].ppid);
+        // if (strncmp(list[i].name, "bash", strlen("bash")) == 0 && list[i].pid ==1319) {
+        //     printf("\n\n@%sAWESOME!!!!i:%dpid:%d ppid:%dpidstring:%s#ppidstring:%s\n\n", list[i].name, i, list[i].pid, list[i].ppid, list[i].pidstring, list[i].ppidstring);
+        //     assert(0);
+        // }
+    }
+    for (int i = 0; i < nn; i++) {
+        list[i].layer = compute_layer(&list[i], list);
+    }
+
+    if (pstree_numeric_sort == false) {
+        struct data tmp;
+        for (int i = 0; i < nn - 1; i++) {
+            for (int j = 0; j < nn - 1 - i; j++) {
+                if (list[j].layer > list[j + 1].layer || (list[j].layer == list[j + 1].layer && strcmp(list[j].name, list[j + 1].name) > 0) ) {
+                    swap_data(&list[j], &list[j + 1]);
+                }
+            }
+        }
+
+        struct Node Nodelist[nn];
+        memset(&Nodelist, 0, nn*sizeof(struct Node));
+        for (int i = 0; i < nn; i++) {
+            copy_data(&Nodelist[i].item, &list[i]);
+            Nodelist[i].layer = Nodelist[i].item.layer;
+            if (Nodelist[i].item.ppid == 0) {
+                Nodelist[i].parent = NULL;
+                Nodelist[i].brother = NULL;
+            }
+        }
+
+        for (int i = 0; i < nn; i++) {
+            if (Nodelist[i].item.ppid == 0)
+                continue;
+            else {
+                for (int j = 0; j < nn; j++) {
+                    if (Nodelist[j].item.pid == Nodelist[i].item.ppid) {
+                        Nodelist[i].parent = &Nodelist[j];
+                        if (Nodelist[j].child == NULL) {
+                            Nodelist[j].child = &Nodelist[i];
+                        } else {
+                            struct Node* ptrNode;
+                            ptrNode = Nodelist[j].child;
+                            while (ptrNode->brother != NULL) {
+                                ptrNode = ptrNode->brother;
+                            }
+                            ptrNode->brother = &Nodelist[i];
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // for (int i = 0; i < nn; i++) {
+        //     printf("layer:%d\t(pid%d)(ppid%d)%s",list[i].layer, list[i].pid, list[i].ppid, list[i].name);
+        // }
+        int start = 0;
+        for (int i = 0; i < nn; i++) {
+            if (Nodelist[i].item.pid == 1)
+                start = i;
+        }
+        print(&Nodelist[start], pstree_show_pids);
+        return 0;
+    }
+    if (pstree_numeric_sort == true) {
+
+        struct data tmp;
+        for (int i = 0; i < nn - 1; i++) {
+            for (int j = 0; j < nn - 1 - i; j++) {
+                if (list[j].layer > list[j + 1].layer || (list[j].layer == list[j+1].layer && list[j].pid > list[j+1].pid)) {
+                    swap_data(&list[j], &list[j + 1]);
+                }
+            }
+        }
+        struct Node Nodelist[nn];
+        memset(&Nodelist, 0, nn*sizeof(struct Node));
+        for (int i = 0; i < nn; i++) {
+            copy_data(&Nodelist[i].item, &list[i]);
+            Nodelist[i].layer = Nodelist[i].item.layer;
+            if (Nodelist[i].item.ppid == 0) {
+                Nodelist[i].parent = NULL;
+                Nodelist[i].brother = NULL;
+            }
+        }
+
+        for (int i = 0; i < nn; i++) {
+            if (Nodelist[i].item.ppid == 0)
+                continue;
+            else {
+                for (int j = 0; j < nn; j++) {
+                    if (Nodelist[j].item.pid == Nodelist[i].item.ppid) {
+                        Nodelist[i].parent = &Nodelist[j];
+                        if (Nodelist[j].child == NULL) {
+                            Nodelist[j].child = &Nodelist[i];
+                        } else {
+                            struct Node* ptrNode;
+                            ptrNode = Nodelist[j].child;
+                            while (ptrNode->brother != NULL) {
+                                ptrNode = ptrNode->brother;
+                            }
+                            ptrNode->brother = &Nodelist[i];
+                        }
+                    }
+                }
+            }
+        }
+
+        print(&Nodelist[0], pstree_show_pids);
+
+    }
+    assert(!argv[argc]);
     return 0;
 }
