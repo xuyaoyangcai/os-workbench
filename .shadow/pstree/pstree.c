f6d883fe62eb8a3082204edb904ef5e9552f63b3
tracer-nju <tracer@nju.edu.cn>
2025-03-18 22:49:09 +0800

    Linux zhang 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:49:09 up 37 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,242 +1,205 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <dirent.h>
-#include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#define N 10000
 
-typedef int bool;
-#define true 1
-#define false 0
+struct Node	{
+    char pid[80];
+    char ppid[80];
+    char name[80];
+}node[N];
+int size;
+int root;
+int pnt;
+int draw[80];
 
-char filename[512], procname[256];
-bool visit[32768];
-bool issort = false, showpid = false;
-
-bool isnumber(char *s)
-{
-    int length = strlen(s);
-    for (int i = 0; i < length; ++i) {
-        if (!isdigit(s[i])) {
-            return false;
-        }
+void test(char *dir, char *filename) {
+    DIR *dp;
+    struct dirent *entry;
+    struct stat statbuf;
+    if((dp = opendir(dir)) == NULL) {
+        printf("Cannot open dir: %s\n", dir);
+        return;
     }
-    return true;
-}
-
-struct Process {
-    int pid, ppid, nson, nthr;
-    char name[512];
-    struct Process *son[128];
-    struct Process *thr[128];
-};
-
-char tmp[2048];
-void getinfo(struct Process *ret, int pid)
-{
-    char childfile[512], statname[512], taskdirname[512];
-    sprintf(statname, "/proc/%d/stat", pid);
-    sprintf(taskdirname, "/proc/%d/task/", pid);
-    sprintf(childfile, "/proc/%d/task/%d/children", pid, pid);
-
-    FILE *fp = fopen(statname, "r");
-    fscanf(fp, "%d", &ret->pid);    // Get pid
-    fscanf(fp, "%s", tmp);  // Get name
-    tmp[strlen(tmp) - 1] = '\0';
-    strcpy(ret->name, tmp + 1);
-    fscanf(fp, "%s", tmp);
-    fscanf(fp, "%d", &ret->ppid);   // Get ppid
-    fclose(fp);
-
-    fp = fopen(childfile, "r");
-    ret->nson = 0;
-    int ch;
-    while ((fscanf(fp, "%d", &ch)) != EOF) {
-        ret->son[ret->nson] = malloc(sizeof(struct Process));
-        getinfo(ret->son[ret->nson], ch);
-        ret->nson++;
-    }
-
-    DIR *dir;
-    struct dirent *ent;
-    ret->nthr = 0;
-    if ((dir = opendir(taskdirname)) != NULL) {
-        while ((ent = readdir(dir)) != NULL) {
-            if (isnumber(ent->d_name)) {
-                int tid = atoi(ent->d_name);
-                if (tid != pid) {
-                    if (showpid){
-                        ret->thr[ret->nthr] =
-                                malloc(sizeof(struct Process));
-                        sprintf(tmp, "{%s}", ret->name);
-                        strcpy(ret->thr[ret->nthr]->name, tmp);
-                        ret->thr[ret->nthr]->pid = tid;
-                        ret->thr[ret->nthr]->nthr =
-                        ret->thr[ret->nthr]->nson = 0;
-                    }
-                    ret->nthr++;
-                }
-                if (!showpid && ret->nthr >0){
-                    ret->thr[0] =
-                            malloc(sizeof(struct Process));
-                    sprintf(tmp, "{%s}", ret->name);
-                    strcpy(ret->thr[0]->name, tmp);
-                    ret->thr[0]->pid = ret->nthr;
-                    ret->nthr = 1;
-                    ret->thr[0]->nthr =
-                    ret->thr[0]->nson = 0;
-                }
-
+    chdir(dir);
+    while ((entry = readdir(dp)) != NULL) {
+        lstat(entry->d_name, &statbuf);
+        if(S_ISDIR(statbuf.st_mode)) {
+            if(strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+                continue;
+            if((entry->d_name[0] >= '0' && entry->d_name[0] <= '9') || strcmp("task", entry->d_name) == 0) {
+                //printf("%s %s\n", name, entry->d_name);
+                char name[80];
+                strcpy(name, filename);
+                strcat(name, "/");
+                strcat(name, entry->d_name);
+                test(entry->d_name, name);
             }
         }
-        closedir(dir);
-    } else
-        assert(0);
-}
-
-bool cpr(char *a, char *b){
-    int lena = strlen(a), lenb = strlen(b);
-    int len = lena < lenb ?lena : lenb;
-    for (int i=0;i<len;++i){
-        if (a[i]>b[i]) return true;
-        if (b[i]>a[i]) return false;
-    }
-    return lena>lenb;
-}
-
-char pre[512] = "";
-int stack[512];
-int head = 0;
-bool isroot = true;
-void search(struct Process *cur, int type, bool isproc)
-{
-    if (isroot) {
-        if (showpid)
-            sprintf(tmp, "%s(%d)", cur->name, cur->pid);
-        else{
-            sprintf(tmp, "%s", cur->name);
-        }
-        isroot = false;
-        stack[++head] = strlen(tmp) + 1;
-    } else {
-        switch (type) {
-            case 0:        //first of one subtree
-                if (showpid)
-                    sprintf(tmp, "-%s(%d)", cur->name, cur->pid);
-                else{
-                    sprintf(tmp, "-%s", cur->name);
-                }
-                stack[head + 1] = stack[head] + strlen(tmp) + 1;
-                head++;
-                break;
-            case -1:
-                pre[stack[head] - 1] = '\\';
-            default:
-                if (showpid)
-                    sprintf(tmp, "%s-%s(%d)", pre, cur->name, cur->pid);
-                else{
-                    sprintf(tmp, "%s-%s", pre, cur->name);
-                }
-                stack[++head] = strlen(tmp) + 1;
-        }
-    }
-
-    printf("%s", tmp);
-    if (type == -1)
-        pre[stack[head - 1] - 1] = ' ';
-
-    for (int i = stack[head - 1]; i < stack[head]; ++i)
-        pre[i] = ' ';
-    pre[stack[head]] = '|';
-
-    switch (cur->nson + cur->nthr) {
-        case 0:
-            printf("\n");
-            break;
-        case 1:
-            printf("--");
-            pre[stack[head]] = ' ';
-            break;
-        default:
-            printf("-+");
-    }
-
-    pre[++stack[head]] = '\0';
-
-    if (isproc) {
-        if (issort){
-            for (int i=0; i<cur->nson; ++i)
-                for (int j=i+1;j<cur->nson; ++j)
-                    if (cur->son[i]->pid>cur->son[j]->pid){
-                        struct Process * temp = cur->son[i];
-                        cur->son[i] = cur->son[j];
-                        cur->son[j] = temp;
+        else {
+            if(strcmp("status", entry->d_name) == 0) {
+                strcat(filename, "/status");
+                //printf("%s\n", filename);
+                int i = 0;
+                bool flag = false;
+                for(; i < strlen(filename); i++)
+                    if(filename[i] == 't' &&
+                       filename[i+1] == 'a' &&
+                       filename[i+2] == 's' &&
+                       filename[i+3] == 'k') {
+                        flag = true;
+                        break;
                     }
-        } else {
-            for (int i=0; i<cur->nson; ++i)
-                for (int j=i+1;j<cur->nson; ++j)
-                    if (cpr(cur->son[i]->name,cur->son[j]->name)){
-                        struct Process * temp = cur->son[i];
-                        cur->son[i] = cur->son[j];
-                        cur->son[j] = temp;
+                char cmp[80];
+                if(flag){
+                    i-=2;
+                    while(i--)
+                        if(filename[i] == '/')
+                            break;
+                    i++;
+                    int j = 0;
+                    for(; filename[i]!='/'; i++,j++) {
+                        cmp[j] = filename[i];
                     }
-        }
-        for (int i = 0; i < cur->nson; ++i) {
-            int ith = i;
-            if (cur->nson > 1 && cur->nthr == 0
-                && cur->nson - 1 == i)
-                ith = -1;
-            search(cur->son[i], ith, true);
-        }
-        if (showpid){
-            for (int i = 0; i < cur->nthr; ++i) {
-                int ith = i + cur->nson;
-                if (cur->nson + cur->nthr > 1
-                    && cur->nthr - 1 + cur->nson == ith)
-                    ith = -1;
-                search(cur->thr[i], ith, false);
+                    cmp[j] = '\0';
+                    //printf("%s\n", cmp);
+                }
+                FILE *fp = fopen(filename, "r");
+                if(fp) {
+                    char temp0[80];
+                    char temp[80];
+                    while(strcmp(temp0, "Name:") != 0)
+                        fscanf(fp, "%s", temp0);
+                    fscanf(fp, "%[^\n]%*c", temp0);
+                    char *tempn = temp0;
+                    while(*tempn == '\t' || *tempn == 32)
+                        tempn++;
+                    //printf("%s\n", tempn);
+                    /*if(strcmp(temp,"gdbus")==0)
+                        printf("%s\n", filename);*/
+                    /*bool flag = true;
+                    for(int i = 0; i < size; i++)
+                        if(strcmp(node[i].name, temp) == 0) {
+                            flag= false;
+                            break;
+                        }
+                    if(!flag){
+                        break;
+                    }*/
+                    while(strcmp(temp, "Pid:") != 0)
+                        fscanf(fp, "%s", temp);
+                    fscanf(fp, "%s", temp);
+                    if(flag && strcmp(cmp, temp) == 0)
+                        break;
+                    if(!flag)
+                        strcpy(node[size].name, tempn);
+                    else{
+                        strcpy(node[size].name, "{");
+                        strcat(node[size].name, tempn);
+                        strcat(node[size].name, "}");
+                    }
+                    strcpy(node[size].pid, temp);
+                    if(strcmp(temp, "1") == 0)
+                        root = size;
+                    while(strcmp(temp, "PPid:") != 0)
+                        fscanf(fp, "%s", temp);
+                    fscanf(fp, "%s", temp);
+                    if(!flag)
+                        strcpy(node[size].ppid, temp);
+                    else
+                        strcpy(node[size].ppid, cmp);
+                    size++;
+                }
+                else {
+                    printf("wrong\n");
+                }
             }
-        } else if (cur->nthr>0){
-            if (cur->thr[0]->pid>1){
-                sprintf(tmp, "%d*[%s]", cur->thr[0]->pid, cur->thr[0]->name);
-                strcpy(cur->thr[0]->name, tmp);
-            }
-            int ith;
-            if (cur->nson==0) ith = 0; else ith = -1;
-            search(cur->thr[0], ith, false);
         }
     }
-    head--;
-    pre[stack[head]] = '\0';
-
+    chdir("..");
+    closedir(dp);
 }
 
-int main(int argc, char *argv[])
-{
-    // getopt
-    int opt;
-    while ((opt = getopt(argc, argv, "Vnp")) != -1) {
-        switch (opt) {
-            case 'V':
-                printf
-                        ("pstree 1.0\nCopyright (C) 2019 Zheng Zangwei\n");
-                return 0;
-            case 'n':
-                issort = true;
-                break;
-            case 'p':
-                showpid = true;
-                break;
-            default:
-                printf("Only -V,-n,-p is available.\n");
-                return -1;
+void printtree(int fa, int len, bool first, bool single, bool last) {
+    if(!first) {
+        int j = 0;
+        for(int i = 0; i < len; i++) {
+            /*	if(i == strlen(node[root].name) + 2)
+                    printf("│");
+                else
+                    printf(" ");*/
+            while(j < 80) {
+                if(draw[j] == 0)
+                    j++;
+                else
+                    break;
+            }
+            if(j < pnt && draw[j] == i) {
+                printf("│");
+                j++;
+            }
+            else
+                printf(" ");
         }
     }
-
-    struct Process *root = malloc(sizeof(struct Process));
-    getinfo(root, 1);
-    search(root, 0, true);
+    if(fa != root) {
+        if(single)
+            printf("───%s", node[fa].name);
+        else if(first)
+            printf("─┬─%s", node[fa].name);
+        else if(last)
+            printf(" └─%s", node[fa].name);
+        else
+            printf(" ├─%s", node[fa].name);
+    }
+    else
+        printf("%s", node[fa].name);
+    int cunt = 0;
+    int temp = 0;
+    for(int i = 0; i < size; i++) {
+        if(strcmp(node[fa].pid, node[i].ppid) == 0) {
+            cunt++;
+            temp = i;
+        }
+    }
+    if(last && cunt > 1 && pnt>0 && (draw[pnt - 1] - len) == 1) {
+        //printf("%d %d %d", draw[pnt-1], len, pnt);
+        draw[pnt - 1] = 0;
+    }
+    len += strlen(node[fa].name);
+    draw[pnt] = len + 1;
+    if(fa != root)
+        draw[pnt] += 3;
+    pnt++;
+    if(fa == root)
+        len -= 3;
+    bool flag = true;
+    for(int i = 0; i < size; i++) {
+        if(strcmp(node[fa].pid, node[i].ppid) == 0) {
+            if(cunt == 1)
+                printtree(i, len + 3, flag, true, false);
+            else if(i == temp) {
+                printtree(i, len + 3, flag, false, true);
+            }
+            else
+                printtree(i, len + 3, flag, false, false);
+            if(flag)
+                flag = false;
+        }
+    }
+    pnt--;
+    if(flag)
+        printf("\n");
+}
+int main(int argc, char *argv[]) {
+    test("/proc","/proc");
+    //for(int i = 0; i < size; i++)
+    //printf("%s %s %s\n", node[i].pid, node[i].name, node[i].ppid);
+    printtree(root, 0, true, false, false);
     return 0;
 }
