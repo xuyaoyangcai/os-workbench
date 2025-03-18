adc5adad25fb3ef3fb07ceeb5ae1e19df457b7d1
tracer-nju <tracer@nju.edu.cn>
2025-03-18 22:46:07 +0800

    Linux zhang 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:46:07 up 34 min,  1 user,  load average: 0.00, 0.00, 0.00

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,79 +1,149 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
+#include <assert.h>
+#include <getopt.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <limits.h>
+#include <dirent.h>
+#include <stdbool.h>
 
-#define MAX_PROCS 1024
+#define MAXPID_T 100000
 
-typedef struct Process {
+struct proc_node {
     pid_t pid;
     pid_t ppid;
-    char name[256];
-} Process;
+    char filename[20];
+};
 
-Process procs[MAX_PROCS];
-int proc_count = 0;
-
-void read_proc() {
-    DIR *dir;
-    struct dirent *entry;
-
-    if ((dir = opendir("/proc")) == NULL) {
-        perror("opendir(/proc) failed");
-        exit(EXIT_FAILURE);
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_DIR) {
-            char *endptr;
-            pid_t pid = strtol(entry->d_name, &endptr, 10);
-            if (*endptr == '\0') {  // if it's a number
-                char path[256];
-                sprintf(path, "/proc/%d/stat", pid);
-
-                FILE *stat_file = fopen(path, "r");
-                if (stat_file) {
-                    Process p;
-                    p.pid = pid;
-                    fscanf(stat_file, "%d %s %*c %d", &p.pid, p.name, &p.ppid);
-                    fclose(stat_file);
-                    procs[proc_count++] = p;
-                }
-            }
-        }
-    }
-
-    closedir(dir);
-}
-
-void print_tree(pid_t ppid, int indent, int is_last) {
-    for (int i = 0; i < proc_count; i++) {
-        if (procs[i].ppid == ppid) {
-            for (int j = 0; j < indent; j++) {
-                if (j == indent - 1) {
-                    printf("%s", is_last ? "└──" : "├──");
-                } else {
-                    printf("│   ");
-                }
-            }
-            printf("%s\n", procs[i].name);
-
-            int has_child = 0;
-            for (int k = 0; k < proc_count; k++) {
-                if (procs[k].ppid == procs[i].pid) {
-                    has_child = 1;
-                    break;
-                }
-            }
-            print_tree(procs[i].pid, indent + 1, !has_child);
-        }
-    }
-}
+void printPsTree(struct proc_node *procs, pid_t pid, int level, int ops);
+int listProc(struct proc_node *procs);
+bool isNum(char *s);
 
 int main(int argc, char *argv[]) {
-    read_proc();
-    print_tree(1, 0, 0);  // 从init进程（PID 1）开始
+    int ops = 0;
+    static struct option long_options[] = {
+            {"show-pids",       no_argument,    NULL,  'p'},
+            {"numeric-sort",    no_argument,    NULL,  'n'},
+            {"version",         no_argument,    NULL,  'V'},
+            {NULL,              0,              NULL,   0}
+    };
+    char opt;
+    struct proc_node procs[MAXPID_T];
+    for (int i = 0; i < argc; i++) {
+        assert(argv[i]);
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    assert(!argv[argc]);
+    while(-1 != (opt = getopt_long(argc, argv, "pnV", long_options, NULL))){
+        switch(opt) {
+            case 'p':
+                ops = ops | 0x1;
+                break;
+            case 'n':
+                ops = ops | 0x2;
+                break;
+            case 'V':
+                ops = ops | 0x4;
+                break;
+            default:
+                printf("wrong argument\n");
+                break;
+        }
+    }
+    if (listProc(procs) == 0) {
+        switch (ops) {
+            case 0:
+                printf("%s\n", procs[1].filename);
+                printPsTree(procs, 1, 1, ops);
+                break;
+            case 2:
+                printf("%s\n", procs[1].filename);
+                printPsTree(procs, 1, 1, ops);
+                break;
+            case 4:
+            case 6:
+                printf("pstree (PSmisc) UNKNOWN\nCopyright (C) 1993-2019 Werner Almesberger and Craig Small\n\nPSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under\nthe terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING.\n");
+                if (ops == 6) {
+                    printf("%s\n", procs[1].filename);
+                    printPsTree(procs, 1, 1, ops);
+                }
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+                if (ops >= 5) {
+                    printf("pstree (PSmisc) UNKNOWN\nCopyright (C) 1993-2019 Werner Almesberger and Craig Small\n\nPSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under\nthe terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING.\n");
+                }
+                printf("%s(%d)\n", procs[1].filename, 1);
+                printPsTree(procs, 1, 1, ops);
+                break;
+            default:
+                break;
+        }
+    } else {
+        exit(1);
+    }
     return 0;
+}
+
+void printPsTree(struct proc_node *procs, pid_t pid, int level, int ops) {
+    for (int i = pid; i < MAXPID_T; i++) {
+        if (procs[i].ppid == pid) {
+            for (int j = 0; j < level; j++) {
+                printf("\t");
+            }
+            if (ops & 0x1) {
+                printf("%s(%d)\n", procs[i].filename, i);
+            } else {
+                printf("%s\n", procs[i].filename);
+            }
+            printPsTree(procs, i, level + 1, ops);
+        }
+    }
+    return;
+}
+
+int listProc(struct proc_node *procs) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir("/proc/");
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            if (isNum(dir->d_name)) {
+                char filename[300];
+                int pid, ppid;
+                char exe[256];
+                snprintf(filename, sizeof(filename), "/proc/%s/stat", dir->d_name);
+                FILE *fp = fopen(filename, "r");
+                if (fp) {
+                    fscanf(fp, "%d %s %*c %d", &pid, exe, &ppid);
+                    exe[0] = '{';
+                    exe[strlen(exe)-1] = '}';
+                    procs[pid].pid = pid;
+                    procs[pid].ppid = ppid;
+                    strcpy(procs[pid].filename, exe);
+                    fclose(fp);
+                } else {
+                    return 1;
+                }
+            }
+        }
+        closedir(d);
+    }
+    return 0;
+}
+
+bool isNum(char *s) {
+    if (s == NULL) {
+        return false;
+    }
+    for (int i = 0; i < strlen(s); i++) {
+        if (!(s[i] >= '0' && s[i] <= '9')) {
+            return false;
+        }
+    }
+    return true;
 }
