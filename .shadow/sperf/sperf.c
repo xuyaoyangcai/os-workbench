22eff1a20df3c3ce001a17345867201e7346801f
tracer-nju <tracer@nju.edu.cn>
2025-06-06 20:50:35 +0800

    Linux zhang 6.6.87.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Apr 21 17:08:54 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  20:50:35 up 31 min,  1 user,  load average: 0.10, 0.06, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,155 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/signal.h>
+#include <sys/syscall.h>
+#include <regex.h>
 #include <string.h>
-#include <assert.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/time.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <time.h>
 
-#define MAX_SYSCALL 1024
-#define BUF_SIZE 512
-extern char **environ;
+int die=0;
 
-struct syscallStat {
-    char name[64];
-    double time;
-};
-
-int cmp(const void *a, const void *b) {
-    double t1 = ((struct syscallStat *)a)->time;
-    double t2 = ((struct syscallStat *)b)->time;
-    return (t1 > t2) ? -1 : (t1 < t2);
+void handler(int sig){
+    die = 1;
+    // printf("done");
+    // wait(NULL);
+    // exit(EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+char paths[256][512];
+int len;
+regex_t storage;
 
-    // Prepare command: strace -T program args...
-    int cmd_len = argc + 3;
-    char **cmdArgs = malloc(sizeof(char *) * cmd_len);
-    cmdArgs[0] = "strace";
-    cmdArgs[1] = "-T";
-    for (int i = 1; i < argc; i++) {
-        cmdArgs[i + 1] = argv[i];
-    }
-    cmdArgs[argc + 1] = NULL;
+void get_path(){
+    char *path = getenv("PATH");
 
-    // pipe for capturing strace stderr
+    int i=0,count = 0, pstart=0;
+
+    for(char *p=path;*p!=0;p++){
+        count++;
+        if(*p==':'){
+
+            strncpy(paths[i], path+pstart, count-1);
+            i++;
+            pstart=p-path+1;
+            count=0;
+        }
+    }
+    len = i;
+}
+
+void match(char *line){
+    size_t nmatch = 2;
+    regmatch_t pmatch[nmatch];
+    int ret=0;
+    if((ret=regexec(&storage, line, nmatch, pmatch, 0))!=REG_NOMATCH){
+
+        char buf2[4096];
+
+        strncpy(buf2, line+pmatch[1].rm_so,pmatch[1].rm_eo);
+        buf2[line+pmatch[1].rm_so,pmatch[1].rm_eo] = 0;
+        printf("%s\n", buf2);
+    }
+}
+
+
+int main(int argc, char *argv[], char *env[]) {
+    regcomp(&storage,"^(.*?)\\(.*?\\) += [0-9?\\-]+.*?$", REG_NEWLINE|REG_EXTENDED);
+
+    signal(SIGCHLD, handler);
+
+    get_path();
+
     int pipefd[2];
-    if (pipe(pipefd) < 0) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
 
-    pid_t pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+    if(pipe(pipefd)<0)exit(EXIT_FAILURE);
 
-    if (pid == 0) {
-        // Child process
-        close(pipefd[0]);
-        dup2(pipefd[1], STDERR_FILENO);  // Redirect stderr to pipe
-        int null_fd = open("/dev/null", O_RDWR);
-        dup2(null_fd, STDOUT_FILENO);    // Redirect stdout to /dev/null
-        close(null_fd);
+    int pid;
 
-        // Search strace in PATH
-        char *path_env = getenv("PATH");
-        char *path_copy = strdup(path_env);
-        char *token = strtok(path_copy, ":");
-        while (token) {
-            char fullpath[256];
-            snprintf(fullpath, sizeof(fullpath), "%s/strace", token);
-            execve(fullpath, cmdArgs, environ);
-            token = strtok(NULL, ":");
+
+
+    if((pid=fork())==0){ // child
+
+        argv[0] = "strace";
+        close(pipefd[0]); // close read fd
+        dup2(pipefd[1], STDERR_FILENO);
+
+        for(int i=0;i<len;i++){
+            char buf[512];
+            strcpy(buf,paths[i]);
+
+            if(execve(strcat(buf,"/strace"),argv, env )<0){
+                continue;
+            }
+
         }
 
-        fprintf(stderr, "strace not found in PATH\n");
-        exit(127);
     }
 
-    // Parent process
-    close(pipefd[1]);
-    FILE *fp = fdopen(pipefd[0], "r");
-    if (!fp) {
-        perror("fdopen");
-        exit(EXIT_FAILURE);
-    }
+    close(pipefd[1]); // close write
 
-    struct syscallStat stats[MAX_SYSCALL];
-    int stat_count = 0;
-    double total_time = 0;
 
-    struct timeval last_time, current_time;
-    gettimeofday(&last_time, NULL);
+    char buf[1<<16];
+    char *line;
+    while(die!=1){
 
-    char buf[BUF_SIZE];
-    while (fgets(buf, sizeof(buf), fp)) {
-        char syscall[64];
-        double usec;
+        sleep(1);
+        size_t count;
+        do{
 
-        if (sscanf(buf, "%[^<]<%lf>", syscall, &usec) == 2) {
-            // Trim syscall name to exclude parameters
-            for (int i = 0; syscall[i]; i++) {
-                if (syscall[i] == '(') {
-                    syscall[i] = '\0';
-                    break;
+            count = read(pipefd[0], buf,1<<16);
+            int offect=0;
+            // write(STDOUT_FILENO,buf,count);
+
+            while(offect<count){
+                line = buf+offect;
+                int i=0;
+                for(;line[i]!='\n';i++){
+                    offect++;
                 }
+                offect++;
+                line[i]=0;
+                // printf("%s", line);
+                match(line);
             }
 
-            // Accumulate stats
-            int found = 0;
-            for (int i = 0; i < stat_count; i++) {
-                if (strcmp(stats[i].name, syscall) == 0) {
-                    stats[i].time += usec;
-                    found = 1;
-                    break;
-                }
-            }
 
-            if (!found && stat_count < MAX_SYSCALL) {
-                strncpy(stats[stat_count].name, syscall, sizeof(stats[stat_count].name) - 1);
-                stats[stat_count].time = usec;
-                stat_count++;
-            }
 
-            total_time += usec;
-        }
 
-        // Check time
-        gettimeofday(&current_time, NULL);
-        if (current_time.tv_sec != last_time.tv_sec) {
-            qsort(stats, stat_count, sizeof(struct syscallStat), cmp);
-            for (int i = 0; i < 5 && i < stat_count; i++) {
-                int percent = (int)((stats[i].time / total_time) * 100);
-                printf("%s (%d%%)\n", stats[i].name, percent);
-            }
-            printf("==================\n");
-            for (int i = 0; i < 80; i++) putchar('\0');
-            fflush(stdout);
-            gettimeofday(&last_time, NULL);
-        }
+        }while(count);
+
+
+        if(waitpid(pid, NULL,WNOHANG )==pid)exit(0);
+
+
     }
 
-    // Final output
-    qsort(stats, stat_count, sizeof(struct syscallStat), cmp);
-    for (int i = 0; i < 5 && i < stat_count; i++) {
-        int percent = (int)((stats[i].time / total_time) * 100);
-        printf("%s (%d%%)\n", stats[i].name, percent);
-    }
-    printf("==================\n");
-    for (int i = 0; i < 80; i++) putchar('\0');
-    fflush(stdout);
 
-    wait(NULL); // Wait for child
-    free(cmdArgs);
-    return 0;
+
 }
